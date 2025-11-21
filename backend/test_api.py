c238fb855279531e5dfce69fe32c9bc2078119ae
shanmuka7482 <139422239+shanmuka7482@users.noreply.github.com>
2025-11-21 11:47:45 +0530

    updtaed api.py in backend

---
+++ backend/test_api.py
@@ -0,0 +1,18 @@
+import asyncio
+import websockets
+
+async def test():
+    uri = "ws://127.0.0.1:8000/generate"
+    async with websockets.connect(uri) as ws:
+        await ws.send("Generate detailed PPT on AI Agents")
+
+        while True:
+            try:
+                msg = await ws.recv()
+                print("SERVER:", msg)
+                if msg == "DONE":
+                    break
+            except websockets.exceptions.ConnectionClosed:
+                break
+
+asyncio.run(test())
